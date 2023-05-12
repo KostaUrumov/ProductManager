9ea71bc745ef467230ed1f85c4f8f7c63cc85d21
Kostadin Urumov <koceurumov@gmail.com>
2023-05-12 13:55:15 +0300

    sss

---
+++ ProductManager/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManager.Data;
+using ProductManager.Data.Entities;
+
+namespace ProductManager.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProductController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("AddProduct");
+            }
+
+            context.Products.AddRange(product);
+            context.SaveChanges();
+            return RedirectToAction("AllProducts");
+        }
+
+        
+        public IActionResult AllProducts()
+        {
+            var prods = context
+                .Products
+                .ToList();
+            return View(prods);
+        }
+
+    }
+}
